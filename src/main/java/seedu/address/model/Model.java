b8984d90c1d47fcdb2d22f3655bcdab7693f5910
Andrew <159703254+andrewsoonqn@users.noreply.github.com>
2026-04-02 12:39:41 +0800

    Update Model with tag methods

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -1,11 +1,13 @@
 package seedu.address.model;
 
 import java.nio.file.Path;
+import java.util.Set;
 import java.util.function.Predicate;
 
 import javafx.collections.ObservableList;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.model.person.Person;
+import seedu.address.model.tag.Tag;
 
 /**
  * The API of the Model component.
@@ -79,6 +81,16 @@ public interface Model {
      * The person identity of {@code editedPerson} must not be the same as another existing person in the address book.
      */
     void setPerson(Person target, Person editedPerson);
+
+    /**
+     * Returns true if the given tag already exists in the address book.
+     */
+    boolean hasTag(Tag tag);
+
+    /**
+     * Adds the given tags to the custom tag registry.
+     */
+    void addCustomTags(Set<Tag> tags);
 
     /**
      * Returns an unmodifiable view of the filtered person list
