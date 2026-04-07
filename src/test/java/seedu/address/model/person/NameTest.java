2a093f533d220397bb87fcb755370faabdda43bc
hqzell <167740146+hqzell@users.noreply.github.com>
2026-04-07 18:58:11 +0800

    Allow names with hyphens, slashes, brackets, and apostrophes

--- src/test/java/seedu/address/model/person/NameTest.java
+++ src/test/java/seedu/address/model/person/NameTest.java
@@ -36,6 +36,9 @@ public class NameTest {
         assertTrue(Name.isValidName("peter the 2nd")); // alphanumeric characters
         assertTrue(Name.isValidName("Capital Tan")); // with capital letters
         assertTrue(Name.isValidName("David Roger Jackson Ray Jr 2nd")); // long names
+        assertTrue(Name.isValidName("Tan Rei-ann")); // hyphens
+        assertTrue(Name.isValidName("John Doe s/o Richard Doe")); // slashes
+
     }
 
     @Test
